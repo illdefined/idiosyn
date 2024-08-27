6fc2ee1bb5732ff9a532b473c2373ca9d0a2d03c
Mikael Voss <mvs@nyantec.com>
2024-08-28 00:23:03 +0200

    libuv: Disable tests on RISC-V

--- overlay/fixes.nix
+++ overlay/fixes.nix
@@ -1,5 +1,13 @@
-{ ... }: final: prev: {
+{ nixpkgs, ... }: final: prev: 
+let
+  inherit (nixpkgs.lib) optionalAttrs;
+  inherit (prev.stdenv) hostPlatform;
+in {
   redis = prev.redis.overrideAttrs ({
     doCheck = false;
   });
-}
+} // optionalAttrs hostPlatform.isRiscV64 ({
+  libuv = prev.libuv.overrideAttrs ({
+    doCheck = false;
+  });
+})
