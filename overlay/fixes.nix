ec541767915db9174d575fa0e829ed5de23c00b1
Mikael Voss <mvs@nyantec.com>
2024-08-28 16:02:10 +0200

    psutil: Disable tests on RISC-V

--- overlay/fixes.nix
+++ overlay/fixes.nix
@@ -10,4 +10,12 @@ in {
   libuv = prev.libuv.overrideAttrs ({
     doCheck = false;
   });
+
+  python3 = prev.python3.override {
+    packageOverrides = final: prev: {
+      psutil = prev.psutil.overrideAttrs ({
+        doCheck = false;
+      });
+    };
+  };
 })
