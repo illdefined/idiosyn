6b77d339a128f7cfd5bf00ff20b39e1e5d84c692
Mikael Voss <mvs@nyantec.com>
2024-09-16 21:15:43 +0200

    zeromq: Properly define fall‐back cache line size

--- overlay/fixes.nix
+++ overlay/fixes.nix
@@ -29,6 +29,13 @@ in {
         ++ [ "-DGL_CLAMP=GL_CLAMP_TO_EDGE" ] |> toString;
     };
   });
+
+  zeromq = prev.zeromq.overrideAttrs (prevAttrs: {
+    postPatch = prevAttrs.postPatch or "" + ''
+      substituteInPlace CMakeLists.txt \
+        --replace-fail 'CACHELINE_SIZE EQUAL "undefined"' 'CACHELINE_SIZE STREQUAL "undefined"'
+    '';
+  });
 } // optionalAttrs hostPlatform.isRiscV64 ({
   boehmgc = prev.boehmgc.overrideAttrs (prevAttrs: {
     postPatch = prevAttrs.postPatch or "" + ''
