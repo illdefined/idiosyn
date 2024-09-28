bb38beb912f432021a1e7d871726f80e1f1d202b
Mikael Voss <mvs@nyantec.com>
2024-09-28 13:17:45 +0200

    home/editor: Insert tabs with Ctrl+i

--- home/config/nil/editor.nix
+++ home/config/nil/editor.nix
@@ -29,39 +29,46 @@
         };
       };
 
-      keys.normal = {
-        minus = "command_mode";
+      keys = {
+        normal = {
+          minus = "command_mode";
 
-        r = "move_char_left";
-        n = "move_visual_line_down";
-        t = "move_visual_line_up";
-        h = "move_char_right";
+          r = "move_char_left";
+          n = "move_visual_line_down";
+          t = "move_visual_line_up";
+          h = "move_char_right";
 
-        p = "move_prev_word_start";
-        w = "move_next_word_start";
-        l = "move_next_word_end";
-        P = "move_prev_long_word_start";
-        W = "move_next_long_word_start";
-        L = "move_next_long_word_end";
+          p = "move_prev_word_start";
+          w = "move_next_word_start";
+          l = "move_next_word_end";
+          P = "move_prev_long_word_start";
+          W = "move_next_long_word_start";
+          L = "move_next_long_word_end";
 
-        z = "replace";
-        Z = "replace_with_yanked";
+          z = "replace";
+          Z = "replace_with_yanked";
 
-        s = "paste_after";
-        S = "paste_before";
+          s = "paste_after";
+          S = "paste_before";
 
-        x = "extend_line_below";
-        X = "extend_to_line_bounds";
-        A-X = "shrink_to_line_bounds";
+          x = "extend_line_below";
+          X = "extend_to_line_bounds";
+          A-X = "shrink_to_line_bounds";
 
-        e = "change_selection";
-        A-e = "change_selection_noyank";
+          e = "change_selection";
+          A-e = "change_selection_noyank";
 
-        N = "join_selections";
-        A-N = "join_selections_space";
+          N = "join_selections";
+          A-N = "join_selections_space";
 
-        ";" = "search_next";
-        ":" = "search_prev";
+          ";" = "search_next";
+          ":" = "search_prev";
+        };
+
+        insert = {
+          "C-i" = "smart_tab";
+          "C-S-i" = "insert_tab";
+        };
       };
     };
   };
