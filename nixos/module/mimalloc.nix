19e472e29e32f10ab99879bb454ae0c897f5676a
Mikael Voss <mvs@nyantec.com>
2024-10-08 22:22:27 +0200

    nixos/mimalloc: Tune allocator

--- nixos/module/mimalloc.nix
+++ nixos/module/mimalloc.nix
@@ -1,4 +1,8 @@
 { ... }: { config, lib, pkgs, ... }: {
   environment.memoryAllocator.provider = "mimalloc";
-  environment.variables.MIMALLOC_LARGE_OS_PAGES = 1;
+  environment.variables = {
+    MIMALLOC_PURGE_DELAY = 50;
+    MIMALLOC_PURGE_DECOMMITS = 0;
+    MIMALLOC_ALLOW_LARGE_OS_PAGES = 1;
+  };
 }
