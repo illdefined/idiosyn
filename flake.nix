df0f1633ef7175955a700d4687e53bcdf408f619
Mikael Voss <mvs@nyantec.com>
2024-10-22 09:09:33 +0200

    Try unstable Lix

--- flake.nix
+++ flake.nix
@@ -6,9 +6,15 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     nur.url = "github:nix-community/NUR";
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 
     lanzaboote = {
