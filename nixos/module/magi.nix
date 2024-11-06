430377185781091f6b3f46a6ddfa32996af00c45
Mikael Voss <mvs@nyantec.com>
2024-11-06 12:37:34 +0100

    nixos/magi: Use platform config for kernel

--- nixos/module/magi.nix
+++ nixos/module/magi.nix
@@ -16,94 +16,27 @@
   ];
 
   boot.kernelPackages = let
-    inherit (linux-hardened.packages.x86_64-linux) default;
-  in pkgs.linuxPackagesFor (default.override {
-    instSetArch = "znver2";
+    inherit (linux-hardened.packages.x86_64-linux) supermicro-h11ssw;
+  in pkgs.linuxPackagesFor (supermicro-h11ssw.override {
     profiles = {
       physical = true;
       dm-crypt = true;
     };
 
     extraConfig = with linux-hardened.lib.kernel; {
-      CPU_SUP_INTEL = false;
-      CPU_SUP_AMD = true;
-      NR_CPUS = 96;
-      AMD_MEM_ENCRYPT = true;
-
-      ACPI_IPMI = true;
-      ACPI_HMAT = true;
-
-      VIRTUALIZATION = true;
-      KVM = true;
-      KVM_AMD = true;
-      KVM_SMM = true;
-
-      NVME_CORE = true;
-      BLK_DEV_NVME = true;
-      NVME_VERBOSE_ERRORS = true;
-      NVME_HWMON = true;
-
-      ATA = true;
-      ATA_VERBOSE_ERROR = true;
-      ATA_ACPI = true;
-      SATA_PMP = true;
-      SATA_AHCI = true;
-      SATA_MOBILE_LPM_POLICY = 1;
-      ATA_SFF = false;
-
       BLK_DEV_MD = true;
       MD_AUTODETECT = true;
       MD_RAID1 = true;
       DM_RAID = true;
 
-      BNXT = true;
-      BNXT_FLOWER_OFFLOAD = true;
-      BNXT_HWMON = true;
       MLX4_EN = true;
       MLX4_CORE_GEN2 = false;
-
-      IPMI_HANDLER = true;
-      IPMI_PANIC_EVENT = true;
-      IPMI_PANIC_STRING = true;
-      IPMI_DEVICE_INTERFACE = true;
-      IPMI_SI = true;
-      IPMI_SSIF = true;
-
-      I2C_PIIX4 = true;
-
-      HWMON = true;
-      SENSORS_K10TEMP = true;
-
-      WATCHDOG = true;
-      WATCHDOG_HANDLE_BOOT_ENABLED = true;
-      WATCHDOG_OPEN_TIMEOUT = 0;
-      WATCHDOG_SYSFS = true;
-      SP5100_TCO = true;
-
-      VIDEO = true;
-      DRM = true;
-      DRM_FBDEV_EMULATION = true;
-      DRM_AST = true;
-
-      EDAC_DECODE_MCE = true;
-      EDAC_AMD64 = true;
-
-      AMD_PTDMA = true;
-      AMD_IOMMU = true;
-
-      INTEL_RAPL = true;
 
       BTRFS_FS = true;
       BTRFS_FS_POSIX_ACL = true;
 
       CEPH_FS = true;
       CEPH_FS_POSIX_ACL = true;
-
-      CRYPTO_DEV_CCP = true;
-      CRYPTO_DEV_CCP_DD = true;
-      CRYPTO_DEV_SP_CCP = true;
-      CRYPTO_DEV_CCP_CRYPTO = true;
-      CRYPTO_DEV_SP_PSP = true;
     };
   });
 
