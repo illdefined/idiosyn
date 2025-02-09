777d087b05a0be50c438bf144939adbe1b4fdef5
Mikael Voss <mvs@nyantec.com>
2025-02-09 12:28:43 +0100

    balthasar: Add ephemeral filesystem configuration stub

--- nixos/config/balthasar.nix
+++ nixos/config/balthasar.nix
@@ -1,6 +1,15 @@
 { self, ... }: { lib, config, pkgs, ... }: {
   imports = with self.nixosModules; [ magi ];
 
+  ephemeral = {
+    enable = true;
+    device = "UUID=";
+    boot = {
+      device = "UUID=";
+      fsType = "vfat";
+    };
+  };
+
   services.ntpd-rs.settings.source = map (address: {
     mode = "server";
     inherit address;
