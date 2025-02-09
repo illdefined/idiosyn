eede93e6b4b5c74a7faf11e2a9bd1dd7da18753a
Mikael Voss <mvs@nyantec.com>
2025-02-09 14:48:47 +0100

    nixos/graphical: Add basic PipeWire configuration

--- nixos/module/graphical.nix
+++ nixos/module/graphical.nix
@@ -14,8 +14,30 @@
   security.polkit.enable = true;
   security.rtkit.enable = true;
 
+  services.avahi.enable = true;
+
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    raopOpenFirewall = true;
+
+    extraConfig = {
+      pipewire = {
+        "10-clock-rate" = {
+          "context.properties" = {
+            "default.clock.rate" = 96000;
+            "default.clock.allowed-rates" = [ 16000 32000 44100 48000 88200 96000 176400 192000 ];
+          };
+        };
+
+        "10-airplay" = {
+          "context.modules" = [
+            {
+              name = "libpipewire-module-raop-discover";
+            }
+          ];
+        };
+      };
+    };
   };
 }
