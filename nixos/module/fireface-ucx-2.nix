791bb1d76315fc28dafa1092dfe8fb35994e3c7c
Mikael Voss <mvs@nyantec.com>
2025-02-17 00:29:04 +0100

    fireface-ucx-2: Double headroom to 64 samples

--- nixos/module/fireface-ucx-2.nix
+++ nixos/module/fireface-ucx-2.nix
@@ -6,7 +6,7 @@
           matches = [ { "node.name" = "~alsa_(input|output).usb-RME_Fireface_UCX_II.*"; } ];
           actions.update-props = {
             "api.alsa.htimestamp" = true;
-            "api.alsa.headroom" = 32;
+            "api.alsa.headroom" = 64;
             "session.suspend-timeout-seconds" = 0;
           };
         }
