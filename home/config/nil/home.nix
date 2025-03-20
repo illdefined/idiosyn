0c27304cb2f53a7ed94e95522829ba895b43d894
Mikael Voss <mvs@nyantec.com>
2025-03-20 09:22:00 +0100

    home: use --wrapped flag for nixpkgs-review wrapper

--- home/config/nil/home.nix
+++ home/config/nil/home.nix
@@ -327,7 +327,7 @@ in {
     extraConfig = ''
       use std/dirs
 
-      def nixpkgs-review [cmd: string, ...args: string] {
+      def --wrapped nixpkgs-review [cmd: string, ...args: string] {
         dirs add ~/dev/nixpkgs
         ^nixpkgs-review $cmd --run $env.SHELL --systems linux ...$args
         dirs drop
