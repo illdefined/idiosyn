f468360209e227ee7efad3bb7424cf7722e2111a
Mikael Voss <mvs@nyantec.com>
2026-03-19 12:57:49 +0100

    qemu: disable PIE on static Aarch64 builds

--- overlay/aarch.nix
+++ overlay/aarch.nix
@@ -1,4 +1,4 @@
-{ nixpkgs, ... }: final: prev: 
+{ nixpkgs, ... }: final: prev:
 let
   inherit (nixpkgs) lib;
   inherit (prev.stdenv) hostPlatform;
@@ -6,4 +6,12 @@ in lib.optionalAttrs hostPlatform.isAarch ({
   umockdev = prev.umockdev.overrideAttrs {
     doCheck = false;
   };
+
+  qemu = if hostPlatform.isStatic
+    then prev.qemu.overrideAttrs (prevAttrs: {
+        configureFlags = prevAttrs.configureFlags or [ ] ++ [
+          "--disable-pie"
+        ];
+      })
+    else prev.qemu;
 })
